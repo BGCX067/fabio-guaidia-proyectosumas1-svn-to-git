c353bbb7739ce998b45b02e2ba360a1d9e27e8bb
Your Name <you@example.com>
2015-12-28 20:41:21 +0600

    init

---
+++ Sumas/src/sumas/Clase2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sumas;
+
+/**
+ *
+ * @author Fabio
+ */
+public class Clase2 {
+
+}
---
+++ Sumas/src/sumas/Main.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sumas;
+
+/**
+ *
+ * @author Fabio
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int a=5;
+        int b=11;
+        int c= a + b;
+
+
+      
+        if (c >= 16){
+         System.out.print("suema = " + c);
+        }else{
+
+          System.out.println("Proyecto sumas");
+          System.out.println("si se pudo");
+          System.out.println("Proyecto sumas");
+          System.out.println("jejejeje");
+           System.out.println("12345");
+
+        }
+        
+
+
+
+
+    }
+
+}
+
